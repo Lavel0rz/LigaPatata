140edfd91ac7a8430e3d0a6bbb6cc2d0e7392438
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2022-09-16 11:15:46 +0200

    LFP

---
+++ funcs.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+
+jugadores = []
+gol = []
+df1 = pd.read_csv('Partido1.csv')
+df2 = pd.read_csv('Partido2.csv')
+Todos = list(set(list(df1['Jugadores']) + list(df2['Jugadores'])))
+for jugador in Todos:
+    if jugador in list(df1['Jugadores']) and jugador in list(df2['Jugadores']):
+        gol.append(df1.groupby(['Jugadores'])['Goles'].sum()[jugador] + df2.groupby(['Jugadores'])['Goles'].sum()[
+            jugador]), jugadores.append(jugador)
+    elif jugador in list(df1['Jugadores']) and jugador not in list(df2['Jugadores']):
+        gol.append(df1.groupby(['Jugadores'])['Goles'].sum()[jugador]), jugadores.append(jugador)
+    else:
+        gol.append(df2.groupby(['Jugadores'])['Goles'].sum()[jugador]), jugadores.append(jugador)
+
+df_goles = pd.DataFrame({'Jugadores': jugadores,
+                         'GolesTotales': gol})
+df_goles.sort_values(by='GolesTotales', ascending=False, inplace=True)
+df_goles.reset_index(inplace=True)
+df_goles.drop('index', axis=1, inplace=True)
+
+mvps = []
+jogadores = []
+for jugador in Todos:
+    if jugador in list(df1['Jugadores']) and jugador in list(df2['Jugadores']):
+        mvps.append(df1.groupby(['Jugadores'])['MVP'].sum()[jugador] + df2.groupby(['Jugadores'])['MVP'].sum()[
+            jugador]), jogadores.append(jugador)
+    elif jugador in list(df1['Jugadores']) and jugador not in list(df2['Jugadores']):
+        mvps.append(df1.groupby(['Jugadores'])['MVP'].sum()[jugador]), jogadores.append(jugador)
+    else:
+        mvps.append(df2.groupby(['Jugadores'])['MVP'].sum()[jugador]), jogadores.append(jugador)
+
+df_mvps = pd.DataFrame({'Jugadores': jogadores,
+                        'MVPTotales': mvps})
+df_mvps.sort_values(by='MVPTotales', ascending=False, inplace=True)
+df_mvps.reset_index(inplace=True)
+df_mvps.drop('index', axis=1, inplace=True)
+
+wins = []
+jogadores = []
+for jugador in Todos:
+    if jugador in list(df1['Jugadores']) and jugador in list(df2['Jugadores']):
+        wins.append(df1.groupby(['Jugadores'])['W/L'].sum()[jugador] + df2.groupby(['Jugadores'])['W/L'].sum()[
+            jugador]), jogadores.append(jugador)
+    elif jugador in list(df1['Jugadores']) and jugador not in list(df2['Jugadores']):
+        wins.append(df1.groupby(['Jugadores'])['W/L'].sum()[jugador]), jogadores.append(jugador)
+    else:
+        wins.append(df2.groupby(['Jugadores'])['W/L'].sum()[jugador]), jogadores.append(jugador)
+df_wins = pd.DataFrame({'Jugadores': jogadores,
+                        'TotalWL': wins})
+df_wins.sort_values(by='TotalWL', ascending=False, inplace=True)
+df_wins.reset_index(inplace=True)
+df_wins.drop('index', axis=1, inplace=True)
---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from funcs import df_goles,df_mvps,df_wins
+df1 = pd.read_csv('Partido1.csv')
+df2 = pd.read_csv('Partido2.csv')
+st.title('Liga Patata')
+
+option = st.sidebar.selectbox('Home',
+                                      ['HOME','Partidos','Estadisticas'])
+if option == 'Partidos':
+    option2 = st.sidebar.selectbox('Partidos',
+                                   ['Jornada1', 'Jornada2'])
+    if option2 == 'Jornada1':
+        st.dataframe(df1)
+    elif option2 == 'Jornada2':
+        st.dataframe(df2)
+
+if option == 'Estadisticas':
+    option3 = st.sidebar.selectbox('Estadisticas Agregadas',
+                                   ['Pichichi', 'MVPs','W/L'])
+    if option3 == 'Pichichi':
+        st.dataframe(df_goles)
+    elif option3 == 'MVPs':
+        st.dataframe(df_mvps)
+    elif option3 == 'W/L':
+        st.dataframe(df_wins)
+        
