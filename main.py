d6023ae10078c29b54f88ab54a1c39fd8c7988e4
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2022-09-20 11:45:33 +0200

    qol update

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import pandas as pd
 from funcs import df_goles,df_mvps,df_wins
 df1 = pd.read_csv('Partido1.csv')
 df2 = pd.read_csv('Partido2.csv')
+st.set_page_config(page_title="LigaPatata", page_icon="soccer", initial_sidebar_state='auto')
 st.title('Liga Patata')
+st.sidebar.image(r"patata.jpg", use_column_width=True)
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('patataimagen.png')
 
 option = st.sidebar.selectbox('Home',
                                       ['HOME','Partidos','Estadisticas'])
