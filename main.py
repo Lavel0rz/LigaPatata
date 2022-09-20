ea2d5a2af7a308ce2da18f14282b9f6e62d1e310
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2022-09-20 13:48:35 +0200

    config

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from funcs import df_goles,df_mvps,df_wins
 df1 = pd.read_csv('Partido1.csv')
 df2 = pd.read_csv('Partido2.csv')
-st.set_page_config(page_title="LigaPatata", page_icon="soccer", initial_sidebar_state='auto')
+st.set_page_config(page_title="LigaPatata", page_icon="soccer", initial_sidebar_state='expanded')
 st.title('Liga Patata')
 st.sidebar.image(r"patata.jpg", use_column_width=True)
 import base64
